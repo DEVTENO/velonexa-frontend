aa4e1bdebf0117bf482b71c23f68ed2c7e0e9947
isfndiar <isfandiaradi21@gmail.com>
2024-07-29 09:50:05 +0700

    feat: type User Detail

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -18,6 +18,18 @@ interface UserProfile {
   profileImage: string;
 }
 
+/**
+ * @typedef {Object} UserDetail - Berisi detail data dari user
+ * @property {string} username - unique name for user.
+ * @property {string} name - name of user.
+ * @property {boolean} isVerify - is user verifiy or not
+ * @property {string} profileImage - The URL of the user's image.
+ * @property {number} countPost -  number of Post
+ * @property {number} countFollowing -  number of following
+ * @property {number} countFollowers -  number of followers
+ * @property {string} bio -  description user
+ */
+
 interface UserDetail {
   username: string;
   name: string;
