46a7865ade2b2e51a5beef756b8cd14be00ee165
mA4rK0 <onlymarko25@gmail.com>
2024-07-31 23:00:34 +0700

    membuat page explore menggunakan grid.

---
+++ src/app/explore/page.jsx
@@ -0,0 +1,16 @@
+import "../explorePage.css";
+
+export default function Explore() {
+  return (
+    <>
+      <section className="explore-content">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </section>
+    </>
+  );
+}
