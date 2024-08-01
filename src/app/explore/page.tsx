a907ba94345b2936bf01997356bd585178254276
mA4rK0 <onlymarko25@gmail.com>
2024-08-01 16:17:30 +0700

    mengubah isi dari page.tsx menggunakan .map() sehingga cocok untuk pengulangan, menambahkan logic untuk design, dan mengubah ukuran section.

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,25 +1,55 @@
 import React from "react";
 import "../explorePage.css";
 
+type Item = {
+  id: number;
+  text: string;
+};
+
 const Explore: React.FC = () => {
+  const items: Item[] = [
+    { id: 1, text: "1" },
+    { id: 2, text: "2" },
+    { id: 3, text: "3" },
+    { id: 4, text: "4" },
+    { id: 5, text: "5" },
+    { id: 6, text: "6" },
+    { id: 7, text: "7" },
+    { id: 8, text: "8" },
+    { id: 9, text: "9" },
+    { id: 10, text: "10" },
+  ];
+
   return (
     <>
       <section className="explore-content">
-        <div className="">
-          <div className="w-full h-full">1</div>
-        </div>
-        <div>2</div>
-        <div className="large-display">3</div>
-        <div>4</div>
-        <div>5</div>
-        <div className="large-display">6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
+        {items.map(({ id, text }) => (
+          <div key={id} className={`${id % 10 === 3 || id % 10 === 6 ? "large-display" : ""}`}>
+            {text}
+          </div>
+        ))}
       </section>
     </>
   );
+
+  // return (
+  //   <>
+  //     <section className="explore-content">
+  //       <div className="">
+  //         <div className="w-full h-full"></div>
+  //       </div>
+  //       <div>2</div>
+  //       <div className="large-display">3</div>
+  //       <div>4</div>
+  //       <div>5</div>
+  //       <div className="large-display">6</div>
+  //       <div>7</div>
+  //       <div>8</div>
+  //       <div>9</div>
+  //       <div>10</div>
+  //     </section>
+  //   </>
+  // );
 };
 
 export default Explore;
