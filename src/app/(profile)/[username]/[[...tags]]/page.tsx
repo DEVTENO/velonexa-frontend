619559588d6a90a78b961c334f3dc6ff3e39c5c8
isfndiar <isfandiaradi21@gmail.com>
2024-08-14 09:34:15 +0700

    refactoring: bug

--- src/app/(profile)/[username]/[[...tags]]/page.tsx
+++ src/app/(profile)/[username]/[[...tags]]/page.tsx
@@ -3,9 +3,8 @@ import Cookies from "js-cookie";
 import { FetchApiResponse, OtherUserPost, UserPosts } from "@/lib/types/types";
 import Image from "next/image";
 import Link from "next/link";
-import UserSaved from "./userSaved";
-import UserReels from "./userReels";
 import UserPost from "./userPost";
+import { notFound } from "next/navigation";
 type ProfileProps = {
   params: {
     tags: string;
@@ -18,20 +17,20 @@ type TokenProps = {
 };
 
 // http://localhost:{{$PORT}}/api/v1/users/me/media?type=posts
-async function getUserPost(): Promise<FetchApiResponse<UserPosts[]>> {
-  const res = await fetch(
-    `http:localhost:3000/api/v1/users/me/media?type=posts`,
-    { cache: "no-store" }
-  );
-  const response: FetchApiResponse<UserPosts[]> = await res.json();
-  return response;
-}
+// async function getUserPost(): Promise<FetchApiResponse<UserPosts[]>> {
+//   const res = await fetch(
+//     `http:localhost:3000/api/v1/users/me/media?type=posts`,
+//     { cache: "no-store" }
+//   );
+//   const response: FetchApiResponse<UserPosts[]> = await res.json();
+//   return response;
+// }
 
 async function getOtherUserPost(
   username: string
 ): Promise<FetchApiResponse<OtherUserPost[]>> {
   const res = await fetch(
-    `http:localhost:3000/api/v1/users/${username}/media?type=posts`,
+    `${process.env.API_URL}/api/v1/users/${username}/media?type=posts`,
     {
       cache: "no-store",
     }
@@ -40,19 +39,15 @@ async function getOtherUserPost(
   return response;
 }
 
-// http://localhost:{{$PORT}}/api/v1/users/me/media - reels
-async function getUserReels() {}
-
-// http://localhost:{{$PORT}}/api/v1/users/me/media/tagged
-async function getUserTagged() {}
-
 const ProfilePage = async ({ params }: ProfileProps) => {
   const { tags, username } = params;
   const token: TokenProps = JSON.parse(
     Cookies.get("token") ?? '{"username": "user-1"}'
   );
-
   // get data user post lain atau yang dicari
+  if (tags !== undefined) {
+    throw notFound();
+  }
   const { data } = await getOtherUserPost(username!);
   return UserPost(data);
 };
