c8bf063f7e4bad618f0a7c915e2fca029795b567
prabowo-saktii <prabowosakti2023@gmail.com>
2024-08-16 07:00:37 +0700

    menambahkan fetch api pada event handler handleSubmit

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,13 +3,7 @@
 import React, { useState } from "react";
 import LogoRegister from "@/components/ui/LogoRegister";
 import FacebookAuth from "@/components/ui/FacebookAuth";
-
-type RegisterFormData = {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
+import { FetchApiResponse, RegisterFormData } from "@/lib/types/types";
 
 const Register: React.FC<RegisterFormData> = () => {
   const [formData, setFormData] = useState<RegisterFormData>({
@@ -18,9 +12,48 @@ const Register: React.FC<RegisterFormData> = () => {
     password: "",
     confirmPassword: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState<string | null>(null);
+  const [activeButton, setActiveButton] = useState<boolean>(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setMessage(null);
+    setActiveButton(false);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    console.log("berhasil klik", formData);
+    try {
+      const response = await fetch("/api/v1/users/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data =
+          (await response.json()) as FetchApiResponse<RegisterFormData>;
+        if (data.success) {
+          setIsLoading(false);
+          setMessage(data.message);
+          console.log(data.data);
+          setFormData({
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+          });
+        }
+      } else {
+        const errorData = await response.json();
+        setMessage(errorData.message);
+      }
+    } catch (error) {}
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
 
   return (
@@ -38,27 +71,39 @@ const Register: React.FC<RegisterFormData> = () => {
         <div>Atau</div>
         <div className="h-px bg-[#DBDBDB] w-[110px]" />
       </div>
-
+      <div>{message}</div>
       <form action="" className="mt-[9px]">
         <input
           className="border border-[#dbdbdb] p-2 text-[13px] focus:outline-none  focus:border-gray-400 mt-1.5 w-[266px] h-[38px] span-2"
           placeholder="Username"
           type="text"
+          id="username"
+          value={formData.username}
+          onChange={handleChange}
         />
         <input
           className="border border-[#dbdbdb] p-2 text-[13px] focus:outline-none  focus:border-gray-400 mt-1.5 w-[266px] h-[38px] span-2"
           placeholder="Email"
           type="text"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
         />
         <input
           className="border border-[#dbdbdb] p-2 text-[13px] focus:outline-none  focus:border-gray-400 mt-1.5 w-[266px] h-[38px] span-2"
           placeholder="Password"
           type="password"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
         />
         <input
           className="border border-[#dbdbdb] mb-2 p-2 text-[13px] focus:outline-none  focus:border-gray-400 mt-1.5 w-[266px] h-[38px] span-2"
           placeholder="Confirm Password"
           type="password"
+          id="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
         />
         <span className="flex flex-col text-[12px] tracking-tight text-balance  text-[#737373] ">
           <span className="">
@@ -70,7 +115,10 @@ const Register: React.FC<RegisterFormData> = () => {
             Policy .
           </span>
         </span>
-        <button className="container text-[14px] leading-[18px] text-white hover:bg-[#1877F2] bg-[#0095F6] w-[17rem] h-[34px] rounded-lg  mt-[18px]">
+        <button
+          onClick={formData.username.length >= 5 ? handleSubmit : ""}
+          className="container text-[14px] leading-[18px] text-white hover:bg-[#1877F2] bg-[#0095F6] w-[17rem] h-[34px] rounded-lg  mt-[18px]"
+        >
           Sign up
         </button>
       </form>
