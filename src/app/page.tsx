962673c38a798884eb7cd489cafe728a7b060e42
mA4rK0 <onlymarko25@gmail.com>
2024-08-25 17:31:55 +0700

    membuat container.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from "react";
+
+const Home: React.FC = () => {
   return (
-    <div>
-      <div>hello</div>
+    <div className="container mx-3.5 my-6 flex flex-col">
+      <div>hello test</div>
     </div>
   );
-}
+};
+
+export default Home;
