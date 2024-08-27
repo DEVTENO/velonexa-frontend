c1b12fbf8f61f6bc22e6cc4fe0325763394d0629
mA4rK0 <onlymarko25@gmail.com>
2024-08-27 19:42:29 +0700

    membuat kerangka design menggunakan border.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,58 +3,62 @@ import { EllipsisVertical, Heart, Share2, Bookmark } from "lucide-react";
 
 const Home: React.FC = () => {
   return (
-    <div className="container relative border-2 border-black mx-1 my-1 flex flex-col w-[40rem] min-h-lvh">
-      <APost />
+    <div className="container relative border-2 border-black ml-52 my-1 flex max-w-[35rem] min-h-lvh">
+      <div className="container border-2 border-red-600 flex my-auto min-w-[30rem] min-h-[42rem]">
+        <APost />
+      </div>
     </div>
   );
 };
 
 const APost: React.FC = () => {
   return (
-    <div className="relative h-full w-full border-2 border-black my-2 min-h-[96vh]">
+    <div className="container relative flex flex-col border-dashed border-2 border-blue-600 rounded-2xl -ml-2 my-auto min-w-[27.5rem] w-full h-full min-h-[40rem]">
       <div className="top-elem">
-        <div className="flex flex-wrap items-center mx-2 mt-2 justify-between">
-          <div className="flex flex-wrap items-center gap-2">
+        <div className="flex flex-wrap items-center justify-between w-[27.1rem] -ml-8 border-2 border-green-500 rounded-xl">
+          <div className="flex flex-wrap items-center gap-2 border-2 border-gray-600 pr-1 rounded-xl">
             <button className="border-2 border-black w-10 h-10 rounded-full"></button>
             <button className="font-bold text-lg">User 1</button>
             <div>•</div>
             <button>10 hours ago</button>
           </div>
-          <button>
-            <EllipsisVertical />
-          </button>
+          <div className="border-2 border-yellow-500 rounded-lg flex justify-center p-2.5">
+            <button>
+              <EllipsisVertical size={17} fill="black" />
+            </button>
+          </div>
         </div>
       </div>
       <div className="image-elem">
-        <div className="relative border-2 my-2 mx-3 h-[27rem] border-black rounded">
+        <div className="relative border-2 w-[27.3rem] min-h-[26.5rem] -ml-8 my-3 border-black rounded-2xl">
           <div className="absolute inset-0"></div>
         </div>
       </div>
       <div className="like-share-save">
-        <div className="flex flex-wrap mx-5 justify-between">
-          <div className="left-elem">
+        <div className="flex flex-wrap justify-between border-2 rounded-md border-purple-600 w-[27.1rem] -ml-8">
+          <div className="left-elem px-2 border-2 rounded-md border-green-600 flex items-center">
             <button>
               <Heart />
             </button>
-            <button className="ml-3">
+            <button className="ml-5">
               <Share2 />
             </button>
           </div>
-          <div className="right-elem">
+          <div className="right-elem px-4 border-2 rounded-md border-green-600 flex items-center">
             <button>
               <Bookmark />
             </button>
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-y-1 mx-5">
+      <div className="flex flex-col w-[27.1rem] -ml-7">
         <div>99999 likes</div>
         <div>Ini adalah deskripsi post</div>
         <div className="text-slate-500 text-sm">see 100 comments</div>
       </div>
       <div className="add-comment">
         <form action="">
-          <input className="mx-5 my-4 px-1 w-[33rem] h-9 border-2 border-black rounded" type="text" placeholder="Comment..." />
+          <input className="border-2 border-black rounded w-[26.8rem] -ml-7 mt-3 px-1" type="text" placeholder="Comment..." />
         </form>
       </div>
     </div>
