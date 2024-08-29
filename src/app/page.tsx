e6d84b22b46c96372bf716a132ce45667ca49392
mA4rK0 <onlymarko25@gmail.com>
2024-08-29 16:17:09 +0700

    membuat icon menjadi lebih interaktif ketika tombol dipencet.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import React from "react";
 import { EllipsisVertical, Heart, Share2, Bookmark } from "lucide-react";
 
@@ -25,6 +27,9 @@ const Home: React.FC = () => {
 };
 
 const APost: React.FC<Item> = ({ userName, timeStamp, totalLike, totalComment }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+
   return (
     <div className="container border-2 border-black rounded-2xl flex my-7 min-w-[30rem] min-h-[42rem]">
       <div className="container relative flex flex-col border-dashed -ml-2 my-auto min-w-[27.5rem] w-full h-full min-h-[40rem]">
@@ -51,16 +56,16 @@ const APost: React.FC<Item> = ({ userName, timeStamp, totalLike, totalComment })
         <div className="like-share-save">
           <div className="flex flex-wrap justify-between w-[27.1rem] -ml-8">
             <div className="left-elem px-2 flex items-center">
-              <button>
-                <Heart />
+              <button onClick={() => setIsLiked(!isLiked)}>
+                <Heart className={`${isLiked ? "fill-red-500 text-red-500 hover:text-red-500" : "hover:text-gray-500"}`} />
               </button>
               <button className="ml-5">
-                <Share2 />
+                <Share2 className="hover:text-gray-500" />
               </button>
             </div>
             <div className="right-elem px-4 flex items-center">
-              <button>
-                <Bookmark />
+              <button onClick={() => setIsSaved(!isSaved)}>
+                <Bookmark className={`${isSaved ? "fill-black text-black hover:text-black" : "hover:text-gray-500"}`} />
               </button>
             </div>
           </div>
@@ -68,11 +73,12 @@ const APost: React.FC<Item> = ({ userName, timeStamp, totalLike, totalComment })
         <div className="flex flex-col w-[27.1rem] -ml-6 mt-2">
           <div>{totalLike} likes</div>
           <div>Ini adalah deskripsi post</div>
-          <div className="text-slate-500 text-sm">see {totalComment} comments</div>
+          <div className="text-slate-500 text-sm cursor-pointer">see {totalComment} comments</div>
         </div>
         <div className="add-comment">
           <form action="">
-            <input className="border-2 border-black rounded w-[25.4rem] -ml-6 mt-3 px-1" type="text" placeholder="Comment..." />
+            {/* <input className="border-2 border-black rounded w-[25.4rem] -ml-6 mt-3 px-1" type="text" placeholder="Comment..." /> */}
+            <textarea rows={1} className="overflow-y-auto border-2 border-black rounded w-[25.4rem] -ml-6 mt-3 p-1 max-h-24" placeholder="Comment..."></textarea>
           </form>
         </div>
       </div>
