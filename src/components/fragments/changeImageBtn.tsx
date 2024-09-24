927a06244603ed29e473dad152f43b92e2fe32df
mA4rK0 <onlymarko25@gmail.com>
2024-09-24 16:41:07 +0700

    modifikasi elemen input dan button.

--- src/components/fragments/changeImageBtn.tsx
+++ src/components/fragments/changeImageBtn.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import { Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useRef } from "react";
 
 const ChangeImageBtn: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleBtn = () => {
+    fileInputRef.current?.click();
+  };
+
   return (
     <>
       <div className="relative">
-        <Button className="flex flex-wrap justify-center gap-x-[10px] border w-[10.625rem] h-[2.688rem] rounded-[0.438rem] active-link my-[4.563rem]">
+        <Button onClick={handleBtn} className="flex flex-wrap justify-center gap-x-[10px] border w-[10.625rem] h-[2.688rem] rounded-[0.438rem] active-link my-[4.563rem] hover:bg-borderButton">
           <p className="text-sm font-medium">Change Image</p>
           <Pencil size={20} />
         </Button>
-        <Input className="absolute opacity-0 top-[4.54rem] w-[10.625rem] h-[2.688rem] rounded-[0.438rem]" id="picture" type="file" />
+        <Input className="absolute invisible top-[4.54rem] w-[10.625rem] h-[2.688rem] rounded-[0.438rem]" id="picture" type="file" ref={fileInputRef} />
       </div>
     </>
   );
