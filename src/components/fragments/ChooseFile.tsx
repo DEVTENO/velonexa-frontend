3844718bca14ab6ff600bbf9abad982dddb333f1
prabowo-saktii <prabowosakti2023@gmail.com>
2024-09-26 12:14:14 +0700

    proses development PostingOverlay

--- src/components/fragments/ChooseFile.tsx
+++ src/components/fragments/ChooseFile.tsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { FilePond, registerPlugin } from "react-filepond";
-import "filepond/dist/filepond.min.css";
+import "/public/static/filepond.min.css";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginImageTransform from "filepond-plugin-image-transform";
+import FilePondPluginImageCrop from "filepond-plugin-image-crop";
+import FilePondPluginImageResize from "filepond-plugin-image-resize";
+import { X } from "lucide-react";
+
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
 registerPlugin(
   FilePondPluginImageExifOrientation,
   FilePondPluginImagePreview,
-  FilePondPluginFileValidateType
+  FilePondPluginFileValidateType,
+  FilePondPluginImageTransform,
+  FilePondPluginImageCrop,
+  FilePondPluginImageTransform,
+  FilePondPluginImageResize
 );
 
 import { Feeds } from "./Feeds";
@@ -26,17 +35,20 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { PostingOverlay } from "./PostingOverlay";
 
 export const ChooseFile = () => {
   const [isHoveredFeed, setIsHoveredFeed] = useState<boolean>(false);
   const [isReelsActive, setIsReelsActive] = useState<boolean>(false);
   const [showFilePond, setShowFilePond] = useState<boolean>(false);
+  const [showPostingOverlay, setShowPostingOverlay] = useState<boolean>(false);
   const [files, setFiles] = useState<any[]>([]);
 
   const router = useRouter();
 
-  const handleContinue = () => {
-    router.push("/caption");
+  const handleContinue = (e: React.FormEvent) => {
+    e.preventDefault();
+    setShowPostingOverlay(true);
   };
 
   function handleReelsActive() {
@@ -80,9 +92,9 @@ export const ChooseFile = () => {
           <AlertDialogHeader className="">
             <AlertDialogTitle className="flex m-auto h-[270px] transition-all  duration-300">
               {isReelsActive
-                ? "Drag & Drop Your Reels"
+                ? "Choose Your Reels"
                 : isHoveredFeed
-                ? "Drag & Drop Your Feeds"
+                ? "Choose Your Feeds"
                 : "Choose One"}
             </AlertDialogTitle>
 
@@ -117,30 +129,56 @@ export const ChooseFile = () => {
               {showFilePond && (
                 <>
                   {isReelsActive ? (
-                    <FilePond
-                      files={files}
-                      allowPaste={true}
-                      onupdatefiles={setFiles}
-                      allowMultiple={false}
-                      allowFileTypeValidation={true}
-                      acceptedFileTypes={["video/*"]}
-                    />
+                    <>
+                      <X
+                        className=" relative  hover:bg-red-100 cursor-pointer ml-auto mr-10 bottom-3"
+                        onClick={allDeactivateButton}
+                        color="red"
+                        size={36}
+                        strokeWidth={2.5}
+                      />
+                      <FilePond
+                        className="relative flex items-center mr-10"
+                        files={files}
+                        allowPaste={true}
+                        onupdatefiles={setFiles}
+                        allowMultiple={false}
+                        allowFileTypeValidation={true}
+                        acceptedFileTypes={["video/*"]}
+                        stylePanelAspectRatio="1:1"
+                      />
+                    </>
                   ) : (
-                    <FilePond
-                      files={files}
-                      allowPaste={true}
-                      onupdatefiles={setFiles}
-                      allowMultiple={false}
-                      allowFileTypeValidation={true}
-                      acceptedFileTypes={["image/*"]}
-                    />
+                    <>
+                      <X
+                        className=" relative  hover:bg-red-100 cursor-pointer ml-auto mr-10 bottom-3"
+                        onClick={allDeactivateButton}
+                        color="red"
+                        size={36}
+                        strokeWidth={2.5}
+                      />
+                      <FilePond
+                        className="relative flex items-center mr-10"
+                        files={files}
+                        allowPaste={true}
+                        onupdatefiles={setFiles}
+                        allowMultiple={false}
+                        allowFileTypeValidation={true}
+                        acceptedFileTypes={["image/*"]}
+                        stylePanelAspectRatio="1:1"
+                      />
+                    </>
                   )}
                 </>
               )}
             </AlertDialogDescription>
           </AlertDialogHeader>
 
-          <AlertDialogFooter className="flex absolute top-[250px] ml-20 gap-[220px] right-[40px] flex-grow justify-evenly ">
+          <AlertDialogFooter
+            className={`flex absolute ${
+              showFilePond ? "top-[600px]" : "top-[250px]"
+            }  ml-20 gap-[220px] right-[40px] flex-grow justify-evenly `}
+          >
             <AlertDialogCancel
               onClick={allDeactivateButton}
               className="mr-[40px] flex"
@@ -148,10 +186,10 @@ export const ChooseFile = () => {
               Cancel
             </AlertDialogCancel>
             <AlertDialogAction
-              className="flex right-6 ml-[50px]"
               onClick={handleContinue}
+              className="flex right-6 ml-[50px]"
             >
-              Continue
+              {showPostingOverlay && <PostingOverlay />}
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
